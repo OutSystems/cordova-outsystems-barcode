004cce48f2f9b44ee31584f392618494f2aefc03
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-11-06 10:14:17 +0000

    misc: bring JS changes from other branch
    References: https://outsystemsrd.atlassian.net/browse/RMET-2758

--- www/OSBarcode.js
+++ www/OSBarcode.js
@@ -1,5 +1,18 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'OSBarcode', 'coolMethod', [arg0]);
-};
+exports.scanBarcode = function (options, successCallback, errorCallback) {
+    options = options || {};
+
+    let scanInstructions = options.scanInstructions;
+    let cameraDirection = options.cameraDirection;
+    let scanOrientation = options.scanOrientation;
+    let scanButton = !!options.scanButton;
+    let scanText = options.scanText;
+    let hint = options.hint;
+    let androidScanningLibrary = options.androidScanningLibrary;
+
+    let args = [{scanInstructions, cameraDirection, scanOrientation, scanButton, scanText, hint, androidScanningLibrary}];
+
+    // temporary, while the bridge doesn't implement the native library connection.
+    console.log(args);
+}
---
+++ www/OSBarcodeConstants.js
@@ -0,0 +1,26 @@
+module.exports = {
+	Hint: {
+		QR_CODE: 0,
+		AZTEC: 1,
+		CODABAR: 2,
+		CODE_39: 3,
+		CODE_93: 4,
+		CODE_128: 5,
+		DATA_MATRIX: 6,
+		MAXICODE: 7,
+		ITF: 8,
+		EAN_13: 9,
+		EAN_8: 10,
+		PDF_417: 11,
+		RSS_14: 12,
+		RSS_EXPANDED: 13,
+		UPC_A: 14,
+		UPC_E: 15,
+		UPC_EAN_EXTENSION: 16,
+		ALL: 17
+	},
+	AndroidScanningLibrary: {
+		ZXING: 'zxing',
+		MLKIT: 'mlkit'
+	}
+}
