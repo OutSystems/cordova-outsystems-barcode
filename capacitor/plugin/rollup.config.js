668fe4cec4383339fabe6bbb449ffece870181a0
Marta Carlos <marta.carlos@outsystems.com>
2024-05-07 10:00:58 +0100

    temporary: trying project with cordova and capacitor

---
+++ capacitor/plugin/rollup.config.js
@@ -0,0 +1,23 @@
+export default {
+  input: 'dist/esm/index.js',
+  output: [
+    {
+      file: 'dist/plugin.js',
+      format: 'iife',
+      name: 'capacitorOSBarcode',
+      globals: {
+        '@capacitor/core': 'capacitorExports',
+        'html5-qrcode': 'html5Qrcode',
+      },
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+    {
+      file: 'dist/plugin.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+  ],
+  external: ['@capacitor/core', 'html5-qrcode'],
+};
