2afcbedfb928410246fb1a5060a0401de0e91448
Marta Carlos <marta.carlos@outsystems.com>
2024-07-19 16:26:41 +0100

    update workflows

--- scripts/change-extensibility.js
+++ scripts/change-extensibility.js
@@ -21,7 +21,7 @@ let url = "https://" + environment + "/CodeUpdater/rest/Bulk/ExtensabilityUpdate
 
 let extensibilityChangeJson = {
     plugin :{
-        url: repository+"#"+branch,
+        url: `https://${repository}#${branch}`,
     },
     metadata: { 
         "mabs-min": mabsMin ? mabsMin : "9.0.0",
@@ -31,7 +31,7 @@ let extensibilityChangeJson = {
 }
 
 console.log(extensibilityChangeJson);
-let extensibilityChangeString = JSON.stringify(extensibilityChangeJson);
+let extensibilityChangeString = JSON.stringify(extensibilityChangeJson, null, '\t');
 let buffer = new Buffer.from(extensibilityChangeString);
 let base64 = buffer.toString('base64');
 
@@ -42,27 +42,32 @@ let body = [{
 
 console.log(
     "Started changing extensibility in module " + pluginSpaceName + 
-    ".\n -- Extensibility will be configured to: " + repository+"#"+branch +
+    ".\n -- Extensibility will be configured to: " + extensibilityChangeJson.plugin.url +
     `\n with forge version ${pluginForgeVersion} and mabs min ${mabsMin}`  +
     "\nin environment:" + environment     
 );
 
 
 
-const response = fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: basicAuthentication
-    },
-    body: JSON.stringify(body), // body data type must match "Content-Type" header
-  }).then((res) => {
-        if(res.ok && res.status == 200)
+async function updateOML() {
+    const response = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: basicAuthentication
+        },
+        body: JSON.stringify(body), // body data type must match "Content-Type" header
+      })
+      
+        if(response.ok && response.status == 200)
             console.log("Successfully updated OML");
-        else console.log(res)
-    })
+        else {
+            let error = await response.text();
+            console.error(error)
+            throw Error("Couldn't update extensibility.")
+        } 
+}
 
-
-
+updateOML()
